6039b0e38578695a0ea06627aada386f4efecc7f
dimkalobov95 <dimkalobov95@gmail.com>
2019-11-08 16:22:57 +0200

    reverted to task sizes

--- src/app/test1/utils.ts
+++ src/app/test1/utils.ts
@@ -1,6 +1,6 @@
 export const PAGE_SIZE = 20;
-export const LIST_SIZE = 1000;
-export const EMPTY_LIST_SIZE = 10000;
+export const LIST_SIZE = 10000;
+export const EMPTY_LIST_SIZE = 1000;
 export const DEBOUNCE_TIME = 500;
 export const DEFAULT_WORDS = 3;
 export const TOTAL_WORDS = 100;
